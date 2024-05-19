19a160af22264a0ce6c1dba8394353e972dd8534
OSBORNE-JUNIOUS\sunam <asaduzzaman.sunam@gmail.com>
2024-05-19 18:54:35 +0600

    second cursor movement added

--- src/components/animation/hero/index.tsx
+++ src/components/animation/hero/index.tsx
@@ -1,28 +1,28 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import anime from "animejs";
+import { MdFilterList } from "react-icons/md";
+import { BsBuildings } from "react-icons/bs";
 
 const HeroAnimation = () => {
+    const [showCursor, setShowCursor] = useState(true)
     useEffect(() => {
-        // const path = anime.path("#demo-svg path");
-        // var path = anime.path(".path");
 
-        // var tl = anime.timeline({
-        //     easing: 'easeOutExpo',
-        //     duration: 750
+        // anime({
+        //     targets: "#cursor",
+        //     translateX: -50,
+        //     rotate: 45,
+        //     duration: 1000,
+        //     loop: false,
+        //     easing: "easeInOutSine",
+        //     direction: "alternate",
+        //     complete: function (anim) {
+        //         console.log(anim.completed);
+        //         if (anim.completed) {
+        //             setShowCursor(false)
+        //         }
+        //     }
         // });
-
-        anime({
-            targets: "#cursor",
-            //   translateX: path("240"),
-            translateX: -50,
-
-            duration: 1000,
-            loop: false,
-            easing: "easeInOutSine",
-            direction: "alternate",
-            autoplay: true
-        });
 
         anime({
             targets: "#target-div",
@@ -40,15 +40,53 @@ const HeroAnimation = () => {
             loop: false,
             translateX: -5,
             translateY: -10,
-            display: "block",
+            // display: "block",
             //   scale:1.1,
         });
 
         anime({
             targets: "#filter-div",
-            opacity:1,
+            opacity: 1,
             delay: 900,
             duration: 1000,
+            // direction:"linear"
+        });
+
+        anime({
+            targets: '#second-cursor',
+            // delay: 1100,
+            delay: 100,
+            duration: 10000,
+            rotate: 45,
+            opacity: 1,
+            keyframes: [
+                { translateX: -10 },
+                { translateY: 60 },
+                { translateX: 105 },
+                { translateY: 60 },
+                { translateX: -10 },
+                { translateY: 120 },
+                { translateX: 105 },
+                { translateY: 120 },
+                { translateX: 160 },
+                { translateY: 10 },
+            ],
+            easing: 'easeOutElastic(1, .8)',
+            // loop:true
+        });
+
+        anime({
+            targets: '#path-svg .path',
+            keyframes: [
+                { translateY: -40 },
+                { translateX: 250 },
+                { translateY: 40 },
+                { translateX: 0 },
+                { translateY: 0 }
+            ],
+            duration: 4000,
+            easing: 'easeOutElastic(1, .8)',
+            loop: true
         });
 
     }, []);
@@ -61,15 +99,46 @@ const HeroAnimation = () => {
                     <div id="target-div" className="absolute bottom-0">
                         🧿
                     </div>
-                    <div id="cursor" className="absolute bottom-0 left-[60px] z-10 origin-center rotate-45">
+                    <div id="cursor" className={`absolute bottom-0 left-[60px] z-10 ${showCursor ? "block" : "hidden"}`}>
                         👈
                     </div>
 
                 </div>
 
                 <div id="filter-menu">
-                    <div id="filter-div" className="w-60 h-72 bg-gray-400 rounded  absolute -left-4 -top-9 opacity-0"></div>
+                    <div id="filter-div" className="w-60 h-72 bg-gray-100 rounded  absolute -left-4 -top-9 opacity-0">
+                        <div className="flex justify-between items-center p-4 space-x-6 border-b-2 border-gray-300">
+                            <MdFilterList />
+                            <h3 className="text-base font-semibold">Filter</h3>
+                        </div>
+                        <div className="flex flex-wrap">
+
+                            <div className="flex justify-center items-baseline space-x-2 p-2 bg-gray-200 m-2 border rounded">
+                                <BsBuildings />
+                                <h3 className="text-base font-semibold">Industry</h3>
+                            </div>
+                            <div className="flex justify-center items-baseline space-x-2 p-2 bg-gray-200 m-2 border rounded">
+                                <BsBuildings />
+                                <h3 className="text-base font-semibold">Industry</h3>
+                            </div>
+                            <div className="flex justify-center items-baseline space-x-2 p-2 bg-gray-200 m-2 border rounded">
+                                <BsBuildings />
+                                <h3 className="text-base font-semibold">Industry</h3>
+                            </div>
+                            <div className="flex justify-center items-baseline space-x-2 p-2 bg-gray-200 m-2 border rounded">
+                                <BsBuildings />
+                                <h3 className="text-base font-semibold">Industry</h3>
+                            </div>
+                        </div>
+                    </div>
                 </div>
+                <div id="second-cursor" className="absolute left-[11px] -top-[23px] opacity-0 origin-center rotate-45">
+                    👈
+                </div>
+
+                <svg id="path-svg" width={1000} height={500} >
+                    <path className="absolute" stroke="red" fill="none" d="M 400 300 L 400 350 L 500 350 L 400 450 L 500 450 L 500 300 Z"></path>
+                </svg>
             </div>
         </>
     );
